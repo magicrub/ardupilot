f9b313d4c9c77c8fa10600404bc8bff0a468bd55
Tom Pittenger <tom.pittenger@KrausAerospace.com>
2021-03-11 16:11:51 -0800

    RC_Channel: add AUX::AIRBRAKE

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -186,6 +186,7 @@ public:
         // inputs eventually used to replace RCMAP
         MAINSAIL =           207, // mainsail input
         FLAP =               208, // flap input
+        AIRBRAKE =           210, // manual airbrake control
     };
     typedef enum AUX_FUNC aux_func_t;
 
