a3e4f6846f34a90bd6350965d1d9dfcfca7185bb
bugobliterator <siddharthbharatpurohit@gmail.com>
2022-02-03 14:44:49 +0530

    AP_Scripting: add socket example

---
+++ libraries/AP_Scripting/examples/socket_test.lua
@@ -0,0 +1,32 @@
+-----------------------------------------------------------------------------
+-- UDP sample: echo protocol server
+-- LuaSocket sample files
+-- Author: Diego Nehab
+-----------------------------------------------------------------------------
+local socket = require("socket")
+host = host or "127.0.0.1"
+port = port or 8888
+if arg then
+    host = arg[1] or host
+    port = arg[2] or port
+end
+gcs:send_text(0, "Binding to host '" ..host.. "' and port " ..port.. "...")
+udp = assert(socket.udp())
+assert(udp:setsockname(host, port))
+assert(udp:settimeout(5))
+ip, port = udp:getsockname()
+assert(ip, port)
+gcs:send_text(0, "Waiting packets on " .. ip .. ":" .. port .. "...")
+function update() -- this is the loop which periodically runs
+	dgram, ip, port = udp:receivefrom()
+	if dgram then
+		gcs:send_text(0, "Echoing '" .. dgram .. "' to " .. ip .. ":" .. port)
+		udp:sendto(dgram, ip, port)
+	else
+        gcs:send_text(0, ip)
+    end
+    return update, 10 -- reschedules the loop
+end
+
+return update() -- run immediately before starting to reschedule
+
