85b2230e0e59afd69eee46440040f1b7555b2040
Graham <graham.gibbons@gmail.com>
2022-03-04 13:06:26 -0800

    Update MAIM page

--- libraries/AP_HAL_ChibiOS/hwdef/H757I_EVAL_intf/scripts/fs/js/button.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#btn-on").click(function () { $.ajax({ url: "/write/0", success: function (result) { console.log(result); } }); });
-    $("#btn-off").click(function () { $.ajax({ url: "/write/1", success: function (result) { console.log(result); } }); });
-    //fetch lates system time every 100ms
-    setInterval(function () {
-        $.ajax({ url: "/read/time", success: function (result) { console.log(result); $("#system-time").html(result.time); } });
-    }, 100);
-});
