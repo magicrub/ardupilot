504dbb3612efb32219dece375d5e3933785b1f30
Tom Pittenger <tom.pittenger@krausaerospace.com>
2022-03-08 10:48:44 -0700

    KHA: added IP:port announcements to UDP streams

--- libraries/AP_HAL_ChibiOS/hwdef/kha_maim/scripts/console1.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/kha_maim/scripts/console1.lua
@@ -4,7 +4,9 @@ local socket = require("socket")
 local stream_id = 1
 
 local ip
+local ip_last
 local port
+local port_last
 local name = kha:get_udp_out_name(stream_id)
 
 local udp = assert(socket.udp())
@@ -14,9 +16,21 @@ function update_ip_port()
     ip = kha:get_udp_out_ip(stream_id)
     port = kha:get_udp_out_port(stream_id)
     assert(ip, port)
+    if (not ip_last) or (not port_last) or (ip ~= ip_last) or (port ~= port_last) then
+        ip_last = ip
+        port_last = port
+        gcs:send_text(0, "UDP Stream " ..name.. " sending to " .. ip .. ":" .. port .. "")
+        periph:can_printf("UDP Stream " ..name.. " sending to " .. ip .. ":" .. port .. "")
+    end
 end
 
+local boot_delay = 1
 function update() -- this is the loop which periodically runs
+    if boot_delay > 0 then
+        boot_delay = 0
+        return update, 1000
+    end
+
     local data = kha:get_udp_out_data_str(stream_id)
     if data then
         update_ip_port()
@@ -26,7 +40,5 @@ function update() -- this is the loop which periodically runs
     return update, interval_ms -- reschedules the loop
 end
 
-update_ip_port()
-gcs:send_text(0, "UDP Stream " ..name.. " sending to " .. ip .. ":" .. port .. "  is running...")
 return update() -- run immediately before starting to reschedule
 
