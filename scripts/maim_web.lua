24e578512aa7dba235bd3e15aaa436cbb651d348
Graham <graham.gibbons@gmail.com>
2022-03-14 14:02:00 -0700

    JSON output again

--- scripts/maim_web.lua
+++ scripts/maim_web.lua
@@ -1,8 +1,4 @@
-local socket = require "socket"
-
-gcs:send_text(0, tostring(math.random()))
-
-local lunajson = require "lunajson"
+local socket = require("socket")
 
 local host = "*"
 local port = 80
@@ -28,13 +24,13 @@ local configuration = {
         enabled = true,
         ip = "127.0.0.1",
         netmask = "255.0.0.0",
-        gateway = "1.1.1.1",
+        gateway = "1.1.1.1"
     },
     payload2 = {
         enabled = true,
         ip = "127.0.0.1",
         netmask = "255.0.0.0",
-        gateway = "1.1.1.1",
+        gateway = "1.1.1.1"
     }
 }
 
@@ -42,7 +38,9 @@ local function update() -- this is the loop which periodically runs
     local connection = socket:accept()
     while true do
         local line, error = connection:receive()
-        if error then break end
+        if error then
+            break
+        end
 
         -- check if we received GET line
         if line:find("GET") then
@@ -50,26 +48,31 @@ local function update() -- this is the loop which periodically runs
             if request_uri == "/" then
                 request_uri = "/index.html"
             end
-        
+
             local path, query = request_uri:match("(.*)(?.*)")
-            if not path then path = request_uri end
+            if not path then
+                path = request_uri
+            end
 
             -- send response
-            local file = io.open("./scripts/fs"..path, "r")
-            if not file then file = io.open("@ROMFS/scripts/fs"..path, "r") end
+            local file = io.open("./scripts/fs" .. path, "r")
+            if not file then
+                file = io.open("@ROMFS/scripts/fs" .. path, "r")
+            end
 
             -- send html file if exists
             if file and path:match(".html") then
                 connection:send("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n")
                 while true do
                     local data = file:read(1024)
-                    if not data then break end
+                    if not data then
+                        break
+                    end
                     connection:send(data)
                 end
 
             elseif path == "/state" then
-                connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nConnection: close\r\n\r\n")
-                connection:send(lunajson.encode({
+                local state = {
                     system = {
                         voltage = math.random(),
                         current = math.random()
@@ -81,13 +84,30 @@ local function update() -- this is the loop which periodically runs
                     payload2 = {
                         voltage = math.random(),
                         current = math.random()
-                    }}))
+                    }
+                }
+                connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nConnection: close\r\n\r\n")
+                connection:send('{ "system": { "voltage": ' .. state.system.voltage .. ', "current": ' ..
+                                    state.system.current .. ' }, "payload1": { "voltage": ' .. state.payload1.voltage ..
+                                    ', "current": ' .. state.payload1.current .. ' }, "payload2": { "voltage": ' ..
+                                    state.payload2.voltage .. ', "current": ' .. state.payload2.current .. ' } }')
 
             elseif path == "/configuration" then
-                connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nConnection: close\r\n\r\n")
-                connection:send(lunajson.encode(configuration))
+                local system = configuration.system
+                local payload1 = configuration.payload1
+                local payload2 = configuration.payload2
 
-            else 
+                connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nConnection: close\r\n\r\n")
+                connection:send(
+                    '{ "system": { "consoleForward": { "ip": "' .. system.consoleForward.ip .. '", "port": ' ..
+                        system.consoleForward.port .. ' }, "maintenancePort": "' .. system.maintenancePort ..
+                        '" }, "payload1": { "enabled": ' .. (payload1.enabled and 'true' or 'false') .. ', "ip": "' ..
+                        payload1.ip .. '", "netmask": "' .. payload1.netmask .. '", "gateway": "' .. payload1.gateway ..
+                        '" }, "payload2": { "enabled": ' .. (payload2.enabled and 'true' or 'false') .. ', "ip": "' ..
+                        payload2.ip .. '", "netmask": "' .. payload2.netmask .. '", "gateway": "' .. payload2.gateway ..
+                        '" } }')
+
+            else
                 -- send 404 if file not found
                 connection:send("HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n")
                 connection:send("<html><body><h1>404 Not Found</h1></body></html>")
