67f2dd2a11a09d7baad22b256616251bc981a188
Tom Pittenger <tom.pittenger@krausaerospace.com>
2022-08-10 08:34:20 -0700

    lua: sanity check airspeed and add arming check

--- libraries/AP_HAL_ChibiOS/hwdef/CubeOrange-KHA/scripts/takeoff_throttle.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/CubeOrange-KHA/scripts/takeoff_throttle.lua
@@ -6,16 +6,19 @@ local rotate_speed = assert(param:get("TKOFF_ROTATE_SPD"),"Lua: Could not read T
 local takeoff_throttle_max = assert(param:get("TKOFF_THR_MAX"),"Lua: Could not read TKOFF_THR_MAX")
 local throttle_max_pwm = assert(param:get("SERVO1_MAX"),"Lua: Could not read SERVO1_MAX")
 local throttle_min_pwm = assert(param:get("SERVO1_MIN"),"Lua: Could not read SERVO1_MIN")
-local AUTO = 10
-local TAKEOFF = 22
+local auth_id = arming:get_aux_auth_id()
+
+local FLIGHT_MODE_PLANE_AUTO = 10
+local MAV_CMD_NAV_TAKEOFF = 22
+local K_THROTTLE = 70
 
 local throttle_scale = throttle_max_pwm - throttle_min_pwm
 
 if takeoff_throttle_max == 0 then
-        takeoff_throttle_max = assert(param:get("THR_MAX"),"Lua: Could not read THR_MAX"
+    takeoff_throttle_max = assert(param:get("THR_MAX"),"Lua: Could not read THR_MAX")
 end
 
-takeoff_throttle_max = takeoff_throttle_max/100
+takeoff_throttle_max = takeoff_throttle_max*0.01
 -- set lower bound to 20% of max throttle when airspeed is zero
 local takeoff_throttle_min = 0.2*takeoff_throttle_max
 -- max throttle is unrestricted when airspeed is 90% of rotate speed
@@ -23,26 +26,37 @@ rotate_speed = rotate_speed*0.9
 
 function update()
 
-    local nav_command = mission:get_current_nav_id()
-    local mode = vehicle:get_mode()
-
-    if (nav_command == TAKEOFF) and (mode == AUTO) and arming:is_armed() then
-        local airspeed = ahrs:airspeed_estimate()
-        local throttle_max = takeoff_throttle_max*math.sqrt((airspeed/rotate_speed)*(1-takeoff_throttle_min)*(1-takeoff_throttle_min))+takeoff_throttle_min
-
-        throttle_max = math.max(throttle_max, takeoff_throttle_min) -- saturate to max of 1.0
-        throttle_max = math.min(throttle_max, takeoff_throttle_max) -- saturate to a min of 0.0
-
-        throttle_max = throttle_scale*throttle_max+throttle_min_pwm -- scale back to pwm values
-        throttle_max = math.floor(throttle_max + 0.5) -- turn into an integer value
-
-        SRV_Channels:set_output_pwm_chan_timeout(1-1,throttle_max,15) -- override throttle channel
-
-        return update, 10
-    else
-        return update, 500
+    -- This scripts only applies to auto-takeoff
+    if (vehicle:get_mode() ~= FLIGHT_MODE_PLANE_AUTO) or (mission:get_current_nav_id() ~= MAV_CMD_NAV_TAKEOFF) then
+        if auth_id then
+            arming:set_aux_auth_passed(auth_id)
+        end
+        return update, 1000
     end
 
+        -- sanity check airspeed sensor
+    local airspeed = ahrs:airspeed_estimate()
+    if (airspeed == nil) then
+        if auth_id then
+            arming:set_aux_auth_failed(auth_id, "Airspeed not available, can not accurately constrain takeoff throttle")
+        end
+        return update, 1000
+    end
+    arming:set_aux_auth_passed(auth_id)
+
+
+    local airspeed = ahrs:airspeed_estimate()
+    local throttle_max = takeoff_throttle_max*math.sqrt((airspeed/rotate_speed)*(1-takeoff_throttle_min)*(1-takeoff_throttle_min))+takeoff_throttle_min
+
+    throttle_max = math.max(throttle_max, takeoff_throttle_min) -- saturate to max of 1.0
+    throttle_max = math.min(throttle_max, takeoff_throttle_max) -- saturate to a min of 0.0
+
+    throttle_max = (throttle_scale*throttle_max) + throttle_min_pwm -- scale back to pwm values
+    throttle_max = math.floor(throttle_max + 0.5) -- turn into an integer value
+
+    SRV_Channels:set_output_pwm_chan_timeout(SRV_Channels:find_channel(K_THROTTLE),throttle_max,15) -- override throttle channel
+
+    return update, 10
 end
 
 return update()
