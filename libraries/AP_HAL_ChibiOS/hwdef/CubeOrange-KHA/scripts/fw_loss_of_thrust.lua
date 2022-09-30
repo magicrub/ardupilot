a2ffcf5a0b7f05896650b4794ea8fd743f4c68c2
Tom Pittenger <tom.pittenger@krausaerospace.com>
2022-09-30 16:37:52 -0700

    script: fix fw_loss_of_throttle

--- libraries/AP_HAL_ChibiOS/hwdef/CubeOrange-KHA/scripts/fw_loss_of_thrust.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/CubeOrange-KHA/scripts/fw_loss_of_thrust.lua
@@ -9,8 +9,15 @@ local case = 0
 local DISARMED = 0
 local MOTOR_OPER = 1
 local MOTOR_INOP_WAITING = 2
-local MOTOR_INOP_ALERT = 3
+local MOTOR_INOP_RECOVERING = 3
+local MOTOR_INOP_ALERT = 4
+local RESET_ESC = 5
+local MOTOR_INOP_RESETTING = 6
 local THROTTLE_MAX = assert(param:get("THR_MAX"),"Lua: Could not read THR_MAX")
+local throttle_srv_chan = assert(SRV_Channels:find_channel(K_THROTTLE), "Could not find K_THROTTLE" .. K_THROTTLE)
+local SERVO_thr_MAX = "SERVO" .. throttle_srv_chan + 1 .. "_MAX"
+local throttle_max_pwm = assert(param:get("SERVO" .. throttle_srv_chan + 1 .. "_MAX"),"Lua: Could not read SERVO" .. throttle_srv_chan + 1 .. "_MAX")
+local throttle_min_pwm = assert(param:get("SERVO" .. throttle_srv_chan + 1 .. "_MIN"),"Lua: Could not read SERVO" .. throttle_srv_chan + 1 .. "_MIN")
 
 -- consider motor stopped when vibe is low and RPM low for more than 4s
 local MOTOR_STOPPED_MS = 4000
@@ -54,6 +61,25 @@ function update()
 
         if (vibe > VIBE_LOW_THRESH) or (throttle < THROTTLE_ON_THRESH) then
             case = MOTOR_OPER
+            --gcs:send_text(0, "K1000: Motor STARTED")
+            return update, 200
+        end
+        if millis() > (motor_out_time + MOTOR_STOPPED_MS) then
+            case = MOTOR_INOP_ALERT
+        end
+        return update, 200
+    end
+    if case == MOTOR_INOP_RECOVERING then
+        if not arming:is_armed() then
+            case = DISARMED
+            return update, 1000
+        end
+
+        local throttle = SRV_Channels:get_output_scaled(K_THROTTLE)
+        local vibe = ahrs:get_vibration():length()
+
+        if (vibe > VIBE_LOW_THRESH) or (throttle < THROTTLE_ON_THRESH) then
+            case = MOTOR_OPER
             gcs:send_text(0, "K1000: Motor STARTED")
             return update, 200
         end
@@ -78,19 +104,19 @@ function update()
             return update, 1000
         end
         gcs:send_text(0, "WARNING: Resetting ESC 1290us")
-        param:set('SERVO1_MAX', 1290)
+        param:set(SERVO_thr_MAX, throttle_min_pwm)
         case = MOTOR_INOP_RESETTING
-        return update, 2000
+        return update, 4000
     end
-    if case == MOTOR_INOP_RESETTING
+    if case == MOTOR_INOP_RESETTING then
         if not arming:is_armed() then
             case = DISARMED
             return update, 1000
         end
         gcs:send_text(0, "WARNING: Resetting ESC 1800us")
-        param:set('SERVO1_MAX', 1800)
-        case = MOTOR_INOP_WAITING
-        return update, 200
+        param:set(SERVO_thr_MAX, throttle_max_pwm)
+        case = MOTOR_INOP_RECOVERING
+        return update, 1500
     end
 
 
