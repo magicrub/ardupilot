37c314776e2375b2d0f69cdfad2de101a6d29532
Ryan Beall <ryan.beall@krausaerospace.com>
2022-11-29 19:11:56 -0500

    LuaScripts: Audit and default.parm update
    Remove redundant params that aren't changed in flight and update defaults.parm to match script

--- libraries/AP_HAL_ChibiOS/hwdef/CubeOrange-KHA/scripts/KHA_param_management.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/CubeOrange-KHA/scripts/KHA_param_management.lua
@@ -5,10 +5,6 @@ local LANDING_PARAMS_SET = false
 
 if param:get("Q_ENABLE") == 0 then
     if not arming:is_armed() then
-        assert(param:set('ARSPD_FBW_MIN',14), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('AHRS_WIND_MAX',25), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('ARSPD_WIND_MAX',25), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('BARO_GND_TEMP',15), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('CAM_TRIGG_DIST',0), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('FS_GCS_ENABL',1), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('FS_LONG_ACTN',1), "K1000: Fixed Wing Preflight Parameter Failed to Write")
@@ -19,17 +15,11 @@ if param:get("Q_ENABLE") == 0 then
         assert(param:set('TECS_SINK_MAX',3), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('TECS_SINK_OPER',0), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('TECS_SPDWEIGHT',1.5), "K1000: Fixed Wing Preflight Parameter Failed to Write")
+        assert(param:set('TECS_TCONST_STE',0.0), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('THR_MAX',70.0), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('THR_MIN',0), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('THR_SLEWRATE',50), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('TKOFF_GND_PITCH',-7), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('TKOFF_ROTATE_SPD',13.5), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('TKOFF_TDRAG_ELEV',-10), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('TKOFF_THR_MAX',63), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('TRIM_ARSPD_CM',1500), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-        assert(param:set('SERVO1_MAX',1800), "K1000: Fixed Wing Landing Parameter Failed to Write")
         assert(param:set('WP_LOITER_RAD',150), "K1000: Fixed Wing Preflight Parameter Failed to Write")
-
     end
     gcs:send_text(4, "K1000: Fixed Wing Preflight Params Set")
 end
@@ -38,22 +28,23 @@ function update()
     local nav_command = mission:get_current_nav_id()
     local mode = vehicle:get_mode()
 
+    if (nav_command == MAV_CMD_NAV_TAKEOFF) and (mode == FLIGHT_MODE_PLANE_AUTO) and arming:is_armed() and TAKEOFF_PARAMS_SET == false then
+        assert(param:set('THR_MAX',70.0), "K1000: Fixed Wing Takeoff Parameter Failed to Write")
+        TAKEOFF_PARAMS_SET = true
+        gcs:send_text(4, "K1000: Fixed Wing Takeoff Params Set")
+    elseif not nav_command == MAV_CMD_NAV_TAKEOFF then
+        TAKEOFF_PARAMS_SET = false
+    end
+
     if (nav_command == MAV_CMD_NAV_LAND) and (mode == FLIGHT_MODE_PLANE_AUTO) and arming:is_armed() and LANDING_PARAMS_SET == false then
-        assert(param:set('LAND_FLARE_ALT',2.5), "K1000: Fixed Wing Landing Parameter Failed to Write")
-        assert(param:set('LAND_PITCH_CD',-50), "K1000: Fixed Wing Landing Parameter Failed to Write")
-        assert(param:set('TECS_LAND_DAMP',10), "K1000: Fixed Wing Landing Parameter Failed to Write")
-        assert(param:set('TECS_LAND_SPDWGT',-1), "K1000: Fixed Wing Preflight Parameter Failed to Write")
         assert(param:set('TECS_CLMB_OPER',0), "K1000: Fixed Wing Landing Parameter Failed to Write")
         assert(param:set('TECS_SINK_OPER',0), "K1000: Fixed Wing Landing Parameter Failed to Write")
         assert(param:set('THR_MAX',80.0), "K1000: Fixed Wing Landing Parameter Failed to Write")
-        assert(param:set('THR_MIN',0), "K1000: Fixed Wing Landing Parameter Failed to Write")
-        assert(param:set('THR_SLEWRATE',50), "K1000: Fixed Wing Landing Parameter Failed to Write")
         LANDING_PARAMS_SET = true
         gcs:send_text(4, "K1000: Fixed Wing Landing Params Set")
     elseif not nav_command == MAV_CMD_NAV_LAND then
         LANDING_PARAMS_SET = false
     end
-
 
     return update, 5000
 end
