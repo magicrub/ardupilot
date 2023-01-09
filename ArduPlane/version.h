714b5eec62e3980b4ca6f6fee38a3da611b62401
Tom Pittenger <tom.pittenger@krausaerospace.com>
2023-01-09 14:37:02 -0800

    KHA: remove version text

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.1.6_jumptag"
+#define THISFIRMWARE "ArduPlane V4.1.6"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,6,FIRMWARE_VERSION_TYPE_OFFICIAL
