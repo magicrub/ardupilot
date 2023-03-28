e94cbad02d0fc2e51ca6bb8336abb7affda089e5
Tom Pittenger <tom.pittenger@krausaerospace.com>
2023-03-29 09:10:27 +1100

    LUA: scripts for http

---
+++ libraries/AP_HAL_ChibiOS/hwdef/kha_maim/scripts/modules/mymath.lua
@@ -0,0 +1,19 @@
+local mymath =  {}
+
+function mymath.add(a,b)
+   return a+b
+end
+
+function mymath.sub(a,b)
+    return a-b
+end
+
+function mymath.mul(a,b)
+    return a*b
+end
+
+function mymath.div(a,b)
+    return a/b
+end
+
+return mymath
